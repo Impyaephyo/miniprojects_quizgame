5e70096d1ed1a5b367e1a711635511d5532fe746
Impyaephyo <77662642+Impyaephyo@users.noreply.github.com>
2021-11-01 20:19:48 +0630

    Initial commit

---
+++ quiz_game.py
@@ -0,0 +1,28 @@
+print("Welcome from Quiz Game")
+
+game = input("Do you want to play ? : ")
+
+if game.lower() == "yes":
+    print("Let's play!!!")
+else:
+    print("Okay byeee!!!")
+
+question = input("What does CPU stand for ? : ")
+if question.lower() == "central processing unit":
+    print("Corret!!!")
+else:
+    print("Incorrect!!!")
+
+question1 = input("What does GPU stand for ? : ")
+if question1.lower() == "graphic processing unit":
+    print("Correct")
+else:
+    print("Incorrect!!!")
+
+question2 = input("What does RAM stand for ? : ")
+if question2.lower() == "random access memory":
+    print("Correct!!!")
+else:
+    print("Incorrect!!!")
+
+print("Thanks for playing!")
