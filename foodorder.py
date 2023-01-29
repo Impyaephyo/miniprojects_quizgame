6e71ae194e3719cc5688b6e0d3c27355b04d67d5
Impyaephyo <mrpyaephyoo.mm@gmail.com>
2023-01-29 15:15:51 +0800

    add code

--- foodorder.py
+++ foodorder.py
@@ -2,3 +2,6 @@ print("Welcome from McDonald's")
 
 menu = ["Promotion Meals","Chicken McCrispy","Sharing","Ala Carte & Value Meals","Sides","Sides","Desserts","Beverages","For the Family"]
 print("Please chooose your menu",menu)
+
+#add comment
+print("Add code")
