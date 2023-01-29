1024df536726a6ec11a41692bb496cccecde23c4
Impyaephyo <mrpyaephyoo.mm@gmail.com>
2023-01-29 20:33:29 +0800

    foodorder.py

--- foodorder.py
+++ foodorder.py
@@ -1,7 +1,37 @@
-print("Welcome from McDonald's")
+from tkinter import *
+root = Tk()
+root.geometry("400x450")
+root.title("Welcome from McDonald's")
 
+lst = Label(root,text="Regular Menu")
+listbox = Listbox(root)
+listbox.insert(1,"Promotion Meals")
+listbox.insert(2,"Chicken McCrispy")
+
+lst.pack()
+listbox.pack()
+root.mainloop()
+
+'''frame = Frame(root)
+frame.pack()
+
+leftframe = Frame(root)
+leftframe.pack(side=LEFT)
+
+bth1 = Button(frame,text="Promotion Meals",fg="red").place(x=30,y=50)
+bth1.pack(side=LEFT)
+
+bth2 = Button(frame,text="Chicken McCrispy",fg="blue").place(x=30,y=90)
+bth2.pack(side=LEFT)
+
+root.mainloop()'''
+
+
+"""print("Welcome from McDonald's")
+
+userquest = str(input("Please choose your order : "))
 menu = ["Promotion Meals","Chicken McCrispy","Sharing","Ala Carte & Value Meals","Sides","Sides","Desserts","Beverages","For the Family"]
 print("Please chooose your menu",menu)
 
 #add comment
-print("Add code")
+print("Add code")"""
